b08232100b9ef02efa98fea8b4a5884b90a4da9f
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-07-09 17:52:41 +0000

    added landing page view

--- shootspot/home/views.py
+++ shootspot/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class LandingPageView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,'landing_page.html')
